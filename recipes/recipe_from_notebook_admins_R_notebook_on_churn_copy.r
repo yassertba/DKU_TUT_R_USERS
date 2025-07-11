9cbdd696492e18f98d1974ebf81fda6236b783cd
admin <admin>
2025-07-11 08:12:15 +0000

    Created r recipe: DKU_TUT_R_USERS.recipe_from_notebook_admins_R_notebook_on_churn_copy

---
+++ recipes/recipe_from_notebook_admins_R_notebook_on_churn_copy.r
@@ -0,0 +1,20 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+library(dataiku)
+library(dplyr)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Read the dataset as a R dataframe in memory
+# Note: here, we only read the first 100K rows. Other sampling options are available
+df <- dkuReadDataset("churn_copy", samplingMethod="head", nbRows=100000)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df %>%
+    rename(Churn = Churn.) %>%
+    mutate(Churn = if_else(Churn == "True.", "True", "False"),
+        Area_Code = as.character(Area_Code)) %>%
+    select(-Phone) ->
+    df_prepared_r
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Recipe outputs
+dkuWriteDataset(churn_prepared_r,"churn_prepared_r")
